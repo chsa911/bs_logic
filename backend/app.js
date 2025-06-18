84ed171a5f87e34a1689f06bb681234f69d0c44f
christopher sanders <ch.sa911@gmail.com>
2025-06-18 16:28:10 +0200

    implement logic to create a BMark in new DB

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const connectDB = require('./config/db');
 const books = require('./models/books');
 const app = express();
-
+let MongoClient = require('mongodb').MongoClient;
+//let url = "mongodb://localhost:27017/";
 // Connect Database
 connectDB();
 app.use(express.json());
@@ -295,8 +296,21 @@ let data = await books.find(
 );
 res.send(data);
 console.log(data);
-});
 
+});
+if (data = 0) {
+MongoClient.connect(url, function(err, db){
+  if (err) throw err;
+  let dbo = db.db("bx");
+  dbo.collection("BM_available").insertOne(BMark, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+console.log(BMark);
+
+  });
+});
+}
 }
 const port = process.env.PORT || 5173;
 
